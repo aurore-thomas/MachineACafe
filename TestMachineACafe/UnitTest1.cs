290fcbc97e423c5aad158c05bef4c2d3b18775db
aurore-thomas <aurorethomas2001@live.fr>
2026-06-08 14:39:11 +0200

    [ADD] First commit

---
+++ TestMachineACafe/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace TestMachineACafe
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
